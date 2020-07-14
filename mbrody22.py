e0754bf3fcc6d490016f64bc878e44ee4eea59bf
Maya Brody <mbrody22@massmutual.com>
2020-07-14 11:08:41 -0400

    added new lines

--- mbrody22.py
+++ mbrody22.py
@@ -5,8 +5,10 @@ def print_in_block(message):
     print("="*4, message)
 
 
+print('\n')
 print_in_block("knock knock!")
 print_in_block("\twho's there?")
 print_in_block("interrupting cow")
 print_in_block("\tinterrupti-")
 print_in_block("MOO!")
+print('\n')
