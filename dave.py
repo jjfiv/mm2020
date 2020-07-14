e81e6cefbaba734a86773d61e2d370a3aaba26a3
Maya Brody <mbrody22@massmutual.com>
2020-07-14 10:48:55 -0400

    more knock knock jokes!

--- dave.py
+++ dave.py
@@ -1,11 +1,30 @@
 def print_in_block(message):
-  """
-  print_in_block prints a message fancy
-  """
-  print("="*4, message)
+    """
+    print_in_block prints a message fancy
+    """
+    print("="*4, message)
+
 
 print_in_block("knock knock!")
 print_in_block("> who's there?")
 print_in_block("orange")
 print_in_block("> orange who?")
 print_in_block("orange you glad...")
+
+print_in_block("knock knock!")
+print_in_block("> who's there?")
+print_in_block("ether")
+print_in_block("> ether who?")
+print_in_block("ether bunny here!")
+
+print_in_block("knock knock!")
+print_in_block("> who's there?")
+print_in_block("Stella")
+print_in_block("> Stella who?")
+print_in_block("Stellanother ether bunny here!")
+
+print_in_block("knock knock!")
+print_in_block("> who's there?")
+print_in_block("cargo")
+print_in_block("> cargo who?")
+print_in_block("cargo beep beep and run over all the ether bunnies!")
