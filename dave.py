4d04d5db671e0fce2cf9fc8a1d0b770d658f118d
DavidKoleczekMM <dkoleczek29@massmutual.com>
2020-07-14 11:06:33 -0400

    remove bad joke

--- dave.py
+++ dave.py
@@ -6,12 +6,6 @@ def print_in_block(message):
 
 
 print_in_block("knock knock!")
-print_in_block("> who's there?")
-print_in_block("orange")
-print_in_block("> orange who?")
-print_in_block("orange you glad...")
-
-print_in_block("knock knock!")
 print_in_block("\twho's there?")
 print_in_block("hatch")
 print_in_block("\thatch who?")
