2ebe7db857847f1ab72609a4452f91d676c3b288
Clara Rosenberg <crosenberg16@massmutual.com>
2020-07-14 10:49:52 -0400

    Update crosenberg16.py

--- crosenberg16.py
+++ crosenberg16.py
@@ -3,7 +3,7 @@ def print_in_block(message):
   print_in_block prints a message fancy
   """
   print("="*4, message)
-  
+
 print("This is going to cause a problem")
 
 print_in_block("knock knock!")
@@ -12,4 +12,4 @@ print_in_block("owls go")
 print_in_block("owls go who?")
 print_in_block("exactly!")
 
-print("katie is the best")
+print("katie is the best") # fun merge error test
