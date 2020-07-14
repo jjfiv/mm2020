28df19dfdb5edb754b4b2c601f4e1eb1fabcb96d
John Foley <jjfoley@smith.edu>
2020-07-14 13:03:42 -0400

    add a pytest? to demo actions

--- jfoley.py
+++ jfoley.py
@@ -9,3 +9,9 @@ print_in_block("who's there?")
 print_in_block("interrupted cow")
 print_in_block("interrupted--")
 print_in_block("MOO!")
+
+def inc(x):
+  return x + 1
+
+def test_answer():
+    assert inc(3) == 4
