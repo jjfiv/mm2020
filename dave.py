095f338e7a7f60a8882b87a0635c38c101b4b8be
elivingston44 <elivingston44@massmutual.com>
2020-07-14 10:50:27 -0400

    add a knock knock joke

--- dave.py
+++ dave.py
@@ -9,3 +9,10 @@ print_in_block("> who's there?")
 print_in_block("orange")
 print_in_block("> orange who?")
 print_in_block("orange you glad...")
+
+
+print_in_block("knock knock!")
+print_in_block("\twho's there?")
+print_in_block("hatch")
+print_in_block("\thatch who?")
+print_in_block("bless you!")
