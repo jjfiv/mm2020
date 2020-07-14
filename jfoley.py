584c7753b459e484a2472656a606bcd444164d16
John Foley <jjfoley@smith.edu>
2020-07-14 07:34:17 -0400

    Create jfoley.py

---
+++ jfoley.py
@@ -0,0 +1,11 @@
+def print_in_block(message):
+  """
+  print_in_block prints a message fancy
+  """
+  print("="*4, message)
+
+print_in_block("knock knock!")
+print_in_block("who's there?")
+print_in_block("interrupted cow")
+print_in_block("interrupted--")
+print_in_block("MOO!")
